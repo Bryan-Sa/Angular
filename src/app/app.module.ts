c6c74a8448015bc150ed550c9f7b6e67b8b68339
Bryan-Sa <bryan.sa@epitech.eu>
2023-01-04 14:31:06 +0100

    Découverte d'angular (cmd, structure, router layout, module, component , ng command)

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { LoginModule } from './login/login.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './src/app/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LoginModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
